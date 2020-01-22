0679f91cdeede8c15de0f7bf513a80d51ff6b075
Vincent Nguyen <vinhbk2000@gmail.com>
2020-01-22 10:54:23 +0700

    clone whatsapp settings

---
+++ iwhatsapp/iwhatsapp/doctype/iwhatsapp_settings/iwhatsapp_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, vinhnguyent090@gmail.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('iWhatsapp Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
