5b0949f5b9e53ab162b313ef2a5cbbd37b97e4cd
Ameer Babu, iWEX Infomatics <emails@iwex.in>
2020-02-22 18:16:26 +0530

    Update iwhatsapp_settings.js

--- iwhatsapp/iwhatsapp/doctype/iwhatsapp_settings/iwhatsapp_settings.js
+++ iwhatsapp/iwhatsapp/doctype/iwhatsapp_settings/iwhatsapp_settings.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2020, vinhnguyent090@gmail.com and contributors
+// Copyright (c) 2020, emails@iwex.in and contributors
 // For license information, please see license.txt
 
 frappe.ui.form.on('iWhatsapp Settings', {
