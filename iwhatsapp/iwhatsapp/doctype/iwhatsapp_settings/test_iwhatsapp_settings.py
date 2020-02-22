a6c3470edb651ddfc05690b0917a3230878b25f0
Ameer Babu, iWEX Infomatics <emails@iwex.in>
2020-02-22 18:27:22 +0530

    Update test_iwhatsapp_settings.py

--- iwhatsapp/iwhatsapp/doctype/iwhatsapp_settings/test_iwhatsapp_settings.py
+++ iwhatsapp/iwhatsapp/doctype/iwhatsapp_settings/test_iwhatsapp_settings.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2020, vinhnguyent090@gmail.com and Contributors
+# Copyright (c) 2020, emails@iwex.in and Contributors
 # See license.txt
 from __future__ import unicode_literals
 
